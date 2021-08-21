9aac2d8a9fb2afd63eb6eb7e3fed84ab9b8a2819
boilinux <stephenjohnwenceslao@gmail.com>
2021-08-21 13:21:11 +0800

    Add filters.

--- lib/widgets/screen/display/display_filters.dart
+++ lib/widgets/screen/display/display_filters.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
 import './drawer/main_drawer.dart';
 
-class DisplayFilters extends StatelessWidget {
+class DisplayFilters extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _DisplayFiltersState createState() => _DisplayFiltersState();
+}
+
+class _DisplayFiltersState extends State<DisplayFilters> {
+  List filters = [
+    {
+      'Gluten-free': false,
+      'Vegetarian': false,
+      'Vegan': false,
+      'Lactose-free': false,
+    }
+  ];
+
+  Widget _buildSwitchListTile(String title, String subtitle, var val) {
+    // bool value = filters[title] as bool;
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      onChanged: (e) {
+        setState(() {
+          if (filters[0][title] == true) {
+            filters[0][title] = false;
+          } else {
+            filters[0][title] = true;
+          }
+        });
+      },
+      value: (filters[0][title] as bool),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,26 +43,37 @@ class DisplayFilters extends StatelessWidget {
         title: Text('Filters'),
       ),
       drawer: MainDrawer(),
-      body: null,
-      // bottomNavigationBar: BottomNavigationBar(
-      //     onTap: _selectPage,
-      //     backgroundColor: Theme.of(context).primaryColor,
-      //     unselectedItemColor: Theme.of(context).accentColor,
-      //     selectedItemColor: Colors.white,
-      //     currentIndex: _selectedPageIndex,
-      //     // type: BottomNavigationBarType.shifting,
-      //     items: [
-      //       BottomNavigationBarItem(
-      //         backgroundColor: Theme.of(context).primaryColor,
-      //         icon: Icon(Icons.category),
-      //         label: 'Categories',
-      //       ),
-      //       BottomNavigationBarItem(
-      //         backgroundColor: Theme.of(context).primaryColor,
-      //         icon: Icon(Icons.star),
-      //         label: 'Favorites',
-      //       ),
-      //     ]),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection.',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchListTile(
+                    'Gluten-free',
+                    'Only included gluten-free meals',
+                    filters.map((e) => e == 'Gluten-free')),
+                _buildSwitchListTile(
+                    'Vegetarian',
+                    'Only included vegetarian meals',
+                    filters.map((e) => e == 'Vegetarian')),
+                _buildSwitchListTile('Vegan', 'Only included vegan meals',
+                    filters.map((e) => e == 'Vegan')),
+                _buildSwitchListTile(
+                    'Lactose-free',
+                    'Only included lactose-free meals',
+                    filters.map((e) => e == 'Lactose-free')),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
